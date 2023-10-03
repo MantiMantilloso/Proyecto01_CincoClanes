04e9b6da73d38577e52114515eff12c9397d724f
Mauricio Mantilla <mauricio.mantilla.ruales@gmail.com>
2023-10-02 23:23:09 -0500

    Armar Constructor para la baraja madre

--- PruebasDeCartas/PruebaDeClases.cpp
+++ PruebasDeCartas/PruebaDeClases.cpp
@@ -29,11 +29,25 @@ public:
     James mato esto :(
 };*/
 
-class baraja{
+class Baraja{
     private:
         vector <string> baraja_madre;
     
     public:
+
+        Baraja(){
+            baraja_madre = {
+            "o1", "*1", "~1", "+1", "!1",
+            "o2", "*2", "~2", "+2", "!2",
+            "o3", "*3", "~3", "+3", "!3",
+            "o4", "*4", "~4", "+4", "!4",
+            "o5", "*5", "~5", "+5", "!5",
+            "o6", "*6", "~6", "+6", "!6",
+            "o7", "*7", "~7", "+7", "!7",
+            "o8", "*8", "~8", "+8", "!8"
+            };
+        }
+
         void mezclar(vector<string>baraja_m);
         void repartir(vector<string> baraja);
 
